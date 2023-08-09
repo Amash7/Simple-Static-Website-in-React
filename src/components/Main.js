6fd2f865f5799d7d4e04c5bc28dcbc31437c7ba1
Amash7 <124393380+Amash7@users.noreply.github.com>
2023-08-09 14:42:50 +0500

    Add files via upload

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Main(){
+    return(
+        <div>
+            <main>
+                <h1>
+                    Fun Facts about React
+                </h1>
+                <ul>
+                    <li>Was first releaed in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100k stars on GitHub</li>
+                    <li>is maintained by Facebook</li>
+                    <li>Powers thousands of enterpise apps, including mobile apps</li>
+                </ul>
+            </main>
+        </div>
+    );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <img src={require('../images/react.png')} alt="react logo" />
+        <h1>ReactFacts</h1>
+        <h2>React Course - Project 1</h2>
+      </nav>
+    </div>
+  );
+}
